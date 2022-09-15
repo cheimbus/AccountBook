5032a7fea633dc0ac48994ca19101047bde981b6
nicesiu <siuh0403@naver.com>
2022-09-16 01:51:53 +0900

    feat:테이블 관계설정

--- ormconfig.ts
+++ ormconfig.ts
@@ -1,17 +1,32 @@
 import { DataSource } from 'typeorm';
 import dotenv from 'dotenv';
 import { Users } from 'src/entities/Users';
+import { AccountBook } from 'src/entities/AccountBook';
+import { RefreshToken } from 'src/entities/RefreshToken';
+import { TodayExpenses } from 'src/entities/TodayExpenses';
 
 dotenv.config();
 
 const dataSource = new DataSource({
   type: 'mysql',
-  host: process.env.TEST_HOST,
-  port: parseInt(process.env.TEST_DB_PORT),
-  username: process.env.TEST_USER_NAME,
-  password: process.env.TEST_PASSWORD,
-  database: process.env.TEST_DATABASE,
-  entities: [Users],
+  host: process.env.TEST === 'true' ? process.env.TEST_HOST : process.env.HOST,
+  port:
+    process.env.TEST === 'true'
+      ? parseInt(process.env.TEST_DB_PORT)
+      : parseInt(process.env.DB_PORT),
+  username:
+    process.env.TEST === 'true'
+      ? process.env.TEST_USER_NAME
+      : process.env.USER_NAME,
+  password:
+    process.env.TEST === 'true'
+      ? process.env.TEST_PASSWORD
+      : process.env.PASSWORD,
+  database:
+    process.env.TEST === 'true'
+      ? process.env.TEST_DATABASE
+      : process.env.DATABASE,
+  entities: [Users, AccountBook, RefreshToken, TodayExpenses],
   migrations: [__dirname + '/src/migrations/*.ts'],
   charset: 'utf8mb4',
   synchronize: false,
